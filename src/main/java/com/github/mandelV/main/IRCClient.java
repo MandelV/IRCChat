b9a895c7de0422287ec9379c31cd8263c4053185
VAUBOURG Mandel <mandel.vaubourg@viacesi.fr>
2018-09-07 13:41:35 +0200

    Edit
    Signed-off-by: VAUBOURG Mandel <mandel.vaubourg@viacesi.fr>

--- src/main/java/com/github/mandelV/main/IRCClient.java
+++ src/main/java/com/github/mandelV/main/IRCClient.java
@@ -31,7 +31,7 @@ public class IRCClient implements Runnable  {
         }
     }
 
-    //test commit
+
     private void connect(){
         this.send("NICK Shinzou");
         this.send("USER Shinzou test localhost :realname");
